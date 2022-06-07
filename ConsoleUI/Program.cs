e0fcdb149e210be727642e76f93d16c9b088cca3
aaronmdenny <aaronmdenny@outlook.com>
2022-06-07 09:46:43 -0400

    define DIP

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,6 +5,10 @@ namespace ConsoleUI
 {
     class Program
     {
+        /*
+         * Dependency Inversion Principle: High-level modules should not depend upon low-level modules, but rather both
+         * should depend upon abstractions. Those abstractions should not depend upon details.
+         */
         static void Main(string[] args)
         {
             Person owner = new Person
